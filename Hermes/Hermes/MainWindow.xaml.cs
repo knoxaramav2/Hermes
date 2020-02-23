dd2348006454dc2b8cf58329caaa9fce488e1293
knoxaramav2 <wtscott@iastate.edu>
2020-02-23 15:09:54 -0600

    Menu skeleton code behind

---
+++ Hermes/Hermes/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Hermes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        /* Menu handlers */
+
+        /***    File     ***/
+        /*      New      */
+        private void NewProject(object sender, RoutedEventArgs e)
+        {
+            
+        }
+        /*      Open     */
+        private void OpenProject(object sender, RoutedEventArgs e)
+        {
+
+        }
+        /*      Save     */
+        private void SaveProject(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ExitApplication(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
