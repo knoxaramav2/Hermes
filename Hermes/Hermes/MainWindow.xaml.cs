b2c51d44acc46bd32a1f8dd8fdf685da30e7b39a
knoxaramav2 <wtscott@iastate.edu>
2020-02-29 15:16:50 -0600

    Map add handler

--- Hermes/Hermes/MainWindow.xaml.cs
+++ Hermes/Hermes/MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Runtime.CompilerServices;
 using System.Windows.Controls;
 using System.Reflection;
 using System.Xml;
+using System.Windows.Media;
 
 namespace Hermes
 {
@@ -24,6 +25,7 @@ namespace Hermes
         private TabControl _leftTabControl;
         private TabControl _rightTabControl;
         private Canvas _canvas;
+        private Window _window;
 
         public MainWindow()
         {
@@ -40,6 +42,7 @@ namespace Hermes
 
         private void BindComponents()
         {
+            _window = (Window)FindName("MWindow");
             _progressBar = (ProgressBar)FindName("ProgressBar");
             _leftTabControl = (TabControl)FindName("LeftTabControl");
             _rightTabControl = (TabControl)FindName("RightTabControl");
@@ -55,11 +58,11 @@ namespace Hermes
             var dialog = new SaveFileDialog
             {
                 InitialDirectory = ProjectFileHandler.GetFormattedUserSpacePath("projects"),
-                Title="New Project"
+                Title = "New Project"
             };
 
             var res = dialog.ShowDialog();
-            if (res==true)
+            if (res == true)
             {
 
             }
@@ -72,7 +75,7 @@ namespace Hermes
             {
                 InitialDirectory = ProjectFileHandler.GetFormattedUserSpacePath("projects"),
                 Filter = "Hermes Projects (*.hrms)|*.hrms",
-                Title="Open Project"
+                Title = "Open Project"
             };
             var res = dialog.ShowDialog();
             if (res == true)
@@ -95,6 +98,63 @@ namespace Hermes
 
         /***    File     ***/
         public void ToggleTabView(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        /***    Settings   ***/
+
+
+        /***    Edit   ***/
+        public void CreateNewMap(object sender, RoutedEventArgs e)
+        {
+            var tabItem = (TabItem)_leftTabControl.Items.GetItemAt(1);
+
+            var listView = (ListView) tabItem.Content;
+            var item = new ListViewItem();
+            var stackPanel = new StackPanel()
+            {
+                Orientation = Orientation.Horizontal
+            };
+
+            var viewButton = new Button()
+            {
+                Content="New Map",
+                HorizontalAlignment=HorizontalAlignment.Stretch,
+                Width=110,
+                MaxWidth=130,
+            };
+
+            viewButton.Click += new RoutedEventHandler(SwitchToMap);
+
+            var delButton = new Button()
+            {
+                Content="-",
+                Width=20,
+                Background=Brushes.Red,
+                Margin = new Thickness(3, 0, 0, 0)
+            };
+
+            delButton.Click += new RoutedEventHandler(DeleteCurrentMap);
+
+            stackPanel.Children.Add(viewButton);
+            stackPanel.Children.Add(delButton);
+
+            item.Content = stackPanel;
+            listView.Items.Add(item);
+            //tabItem.Content = listView;
+
+            //_leftTabControl.Items.Add(tabItem);
+
+
+        }
+
+        public void DeleteCurrentMap(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public void SwitchToMap(object sender, RoutedEventArgs e)
         {
 
         }
